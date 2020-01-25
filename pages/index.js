0cf0040d146469de010cc9f48ed2b263cd36ef4b
dmitry_sidorov <jimmaggot@mail.ru>
2020-01-25 04:54:27 +0300

    Fix indentation

--- pages/index.js
+++ pages/index.js
@@ -3,29 +3,29 @@ import fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
 
 const Index = props => (
-    <Layout>
-        <h1>Batman TV Shows</h1>
-        <ul>
-            {props.shows.map(show => (
-                <li key={show.id}>
-                    <Link href="/p/[id]" as={`/p/${show.id}`}>
-                        <a>{show.name}</a>
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    </Layout>
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(show => (
+        <li key={show.id}>
+          <Link href="/p/[id]" as={`/p/${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
 );
 
-Index.getInitialProps = async function() {
-    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-    const data = await res.json();
+Index.getInitialProps = async function () {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
 
-    console.log('Show data fetched. Count: ', data.length);
+  console.log('Show data fetched. Count: ', data.length);
 
-    return {
-        shows: data.map(entry => entry.show)
-    };
+  return {
+    shows: data.map(entry => entry.show)
+  };
 };
 
 export default Index;
--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,22 +1,22 @@
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
 import Layout from "../../components/Layout";
 
 const Post = props => (
-    <Layout>
-        <h1>{props.show.name}</h1>
-        <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
-        {props.show.image ? <img src={props.show.image.medium} /> : null}
-    </Layout>
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    {props.show.image ? <img src={props.show.image.medium}/> : null}
+  </Layout>
 );
 
-Post.getInitialProps = async function(context) {
-    const { id } = context.query;
-    const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
-    const show = await res.json();
+Post.getInitialProps = async function (context) {
+  const {id} = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
 
-    console.log(`Fetch show: ${show.name}`);
+  console.log(`Fetch show: ${show.name}`);
 
-    return { show };
+  return {show};
 };
 
 export default Post;
